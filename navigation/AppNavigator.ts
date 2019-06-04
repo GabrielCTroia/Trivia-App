fc614b7cb46e66334a49f752f06975a1426bee83
Gabriel C. Troia <catalin.troia@gmail.com>
2019-06-04 17:03:50 -0400

    Remove the orginal Expo Tab Navigator in favor of a simple StackNavigator

--- navigation/AppNavigator.ts
+++ navigation/AppNavigator.ts
@@ -1,10 +1,12 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
+import HomeScreen from '../screens/HomeScreen';
 
-import MainTabNavigator from './MainTabNavigator';
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+  Main: HomeStack,
 }));

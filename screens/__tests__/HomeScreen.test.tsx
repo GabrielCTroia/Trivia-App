f2abfbe951f04ef5362cd0f43a93e8646d928835
Gabriel C. Troia <catalin.troia@gmail.com>
2019-06-04 18:58:59 -0400

    Add HomeScreen Snapshot test

---
+++ screens/__tests__/HomeScreen.test.tsx
@@ -0,0 +1,10 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { HomeScreen } from '../HomeScreen';
+
+it('renders correctly', () => {
+  const tree = renderer.create(<HomeScreen />).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});

f3d069e96f7a8eea81a4a1f814395d3521f1628b
Gabriel C. Troia <catalin.troia@gmail.com>
2019-06-05 18:45:18 -0400

    Set the transition between Screens to be Vertical (per design)

--- navigation/AppNavigator.ts
+++ navigation/AppNavigator.ts
@@ -1,12 +1,42 @@
 import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
 import { HomeScreen } from '../screens/HomeScreen';
+import { QuizScreen } from '../screens/QuizScreen';
+import { Easing, Animated } from 'react-native';
+
+const iosTransitionSpec = {
+  duration: 500,
+  easing: Easing.bezier(0.2833, 0.99, 0.31833, 0.99),
+  timing: Animated.timing,
+}
+
+const transitionConfig = () => {
+  return {
+    transitionSpec: iosTransitionSpec,
+    screenInterpolator: (sceneProps: any) => {
+      const { layout, position, scene } = sceneProps;
+
+      const thisSceneIndex = scene.index;
+      const height = layout.initHeight;
+
+      const translateY = position.interpolate({
+        inputRange: [thisSceneIndex - 1, thisSceneIndex],
+        outputRange: [height, 0],
+      })
+
+      return { transform: [{ translateY }] }
+    },
+  }
+}
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
-});
+  Quiz: {
+    screen: QuizScreen,
+  },
+}, {
+    transitionConfig,
+  });
 
 export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: HomeStack,
 }));

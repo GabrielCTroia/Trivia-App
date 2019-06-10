e0b93d9b602f2cacc0391be617ef3d0598dfd1f0
Gabriel C. Troia <catalin.troia@gmail.com>
2019-06-09 23:47:33 -0400

    Add a Progress Bar to CardStack

--- components/SwipeableCards/CardStack.tsx
+++ components/SwipeableCards/CardStack.tsx
@@ -3,27 +3,27 @@ import R from 'ramda';
 import { View, StyleSheet, ViewProps } from "react-native";
 import { Card } from "./Card";
 import { noop } from "../../util";
+import { ProgressBar } from "./ProgressBar";
+import * as Colors from "../../styles/Colors";
 
 
 export type CardStackProps<T extends { [key: string]: any } = {}> = ViewProps & {
-  // A collections of components and their ids to be rendered as cards
   items: T[];
+  totalItemsCount: number;
+
   keyExtractor: (item: T) => string;
   renderItem: (item: T, index: number) => React.ReactElement | null;
 
   onSwipeLeft?: (cardId: string) => void;
   onSwipeRight?: (cardId: string) => void;
   onEnd?: () => void;
-
-  // This is here only because CardStack doesn't implement it's own indexing
-  totalItemsCount: number;
 }
 
 export function CardStack<T>({ onSwipeLeft = noop, onSwipeRight = noop, onEnd = noop, ...props }: CardStackProps<T>) {
-  const [count, setCount] = useState(props.items.length - 1);
+  const [count, setCount] = useState(0);
 
   const onSwipe = (dir: 'left' | 'right', id: string) => {
-    setCount((prev) => prev - 1);
+    setCount((prev) => prev + 1);
 
     if (dir === 'left') {
       onSwipeLeft(id);
@@ -36,29 +36,47 @@ export function CardStack<T>({ onSwipeLeft = noop, onSwipeRight = noop, onEnd = 
   const firstItems = R.take(2, props.items);
 
   return (
-    <View style={[styles.container, props.style]}>
-      {firstItems.reverse().map((item, index) => {
-        const key = props.keyExtractor(item);
-        const styleName = (index === 0) ? 'backCard' : 'foreCard';
+    <View style={styles.container}>
+      <View style={[styles.cardsContainer, props.style]}>
+        {firstItems.reverse().map((item, index) => {
+          const key = props.keyExtractor(item);
+          const styleName = (index === 0) ? 'backCard' : 'foreCard';
 
-        const getCurrentCardIndex = props.totalItemsCount - (count && count - 1) - index;
-        return (
-          <Card
-            style={(firstItems.length > 1) ? styles[styleName] : styles['foreCard']}
-            key={key}
-            onSwipeLeft={() => onSwipe('left', key)}
-            onSwipeRight={() => onSwipe('right', key)}
-          >
-            {props.renderItem(item, getCurrentCardIndex)}
-          </Card>
-        )
-      })}
+          return (
+            <Card
+              style={[styles.card, (firstItems.length > 1) ? styles[styleName] : styles['foreCard']]}
+              key={key}
+              onSwipeLeft={() => onSwipe('left', key)}
+              onSwipeRight={() => onSwipe('right', key)}
+            >
+              {props.renderItem(item, count + firstItems.length - index)}
+            </Card>
+          )
+        })}
+      </View>
+      <ProgressBar
+        style={styles.progressBar}
+        total={props.totalItemsCount}
+        index={count}
+        color={Colors.warningColor}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignContent: 'center',
+  },
+  cardsContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  card: {
+    height: '80%',
+  },
   foreCard: {
     shadowColor: '#ddd',
     shadowOffset: { width: 0, height: 5 },
@@ -67,13 +85,16 @@ const styles = StyleSheet.create({
   },
   backCard: {
     top: 20,
-    width: '86%',
     left: '7%',
+    width: '86%',
     opacity: .9,
 
     shadowColor: '#ddd',
     shadowOffset: { width: 0, height: 5 },
     shadowRadius: 10,
     shadowOpacity: .5,
+  },
+  progressBar: {
+    justifyContent: 'flex-end',
   },
 });
---
+++ components/SwipeableCards/ProgressBar.tsx
@@ -0,0 +1,66 @@
+import React, { FunctionComponent } from "react";
+import { ViewProps, StyleSheet, View } from "react-native";
+import * as Colors from "../../styles/Colors";
+
+export type ProgressBarProps = ViewProps & {
+  total: number;
+  index: number;
+  color?: string,
+}
+
+export const ProgressBar: FunctionComponent<ProgressBarProps> = ({ color = Colors.baseTextColor, ...props }) => {
+  const dots = Array(props.total).fill('.');
+
+  return (
+    <View style={[styles.container, props.style]}>
+      <View style={styles.dots}>
+        {dots.map((_, i) => (
+          <View
+            key={i}
+            style={[styles.dot, i === props.index && styles.currentDot, { backgroundColor: color }]}
+          />
+        ))}
+      </View>
+    </View>
+  );
+}
+
+const dotSize = 8;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    height: 15,
+  },
+  dots: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  dot: {
+    marginLeft: 3,
+    marginRight: 3,
+    justifyContent: 'center',
+    width: dotSize,
+    height: dotSize,
+    borderRadius: 100 / 2,
+
+    opacity: .5,
+  },
+  currentDot: {
+    opacity: 1,
+    width: dotSize * 1.2,
+    height: dotSize * 1.2,
+    marginTop: - dotSize * .1
+  },
+  closeDot: {},
+  farDot: {
+    width: dotSize * .7,
+    height: dotSize * .7,
+    marginTop: dotSize * .15,
+  },
+  hiddenDot: {
+    display: 'none',
+  },
+});
